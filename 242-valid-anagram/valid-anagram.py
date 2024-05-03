828a042bdbc489b3a23229b9c93d6eaab694e627
Mohamed Esmail <moh.ahmed.esmail.1403@gmail.com>
2024-05-03 20:28:26 +0300

    Time: 49 ms (58.58%) | Memory: 16.9 MB (64.67%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,17 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        char_dict = {}
-        for c in s:
-            if c in char_dict:
-                char_dict[c] += 1
-            else:
-                char_dict[c] = 1
-        for c in t:
-            if c in char_dict:
-                char_dict[c] -= 1
-            else:
-                char_dict[c] = 1
-        for num in char_dict.values():
-            if num != 0:
-                return False
-        return True
+        cnt_s, cnt_t = {}, {}
+        
+        for each_s in s:
+            cnt_s[each_s] = cnt_s.get(each_s, 0) + 1
+
+        for each_t in t:
+            cnt_t[each_t] = cnt_t.get(each_t, 0) + 1
+
+        return cnt_s == cnt_t
