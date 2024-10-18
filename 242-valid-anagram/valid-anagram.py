f06e0780aaa99053b6a4d8c6ecf59fc5f1fb8c58
Mohamed Esmail <moh.ahmed.esmail.1403@gmail.com>
2024-10-18 07:45:37 +0300

    Time: 19 ms (100.00%) | Memory: 16.6 MB (99.94%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,9 +1,16 @@
+from collections import defaultdict
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-
-        for l in list(set(t)):
-            if s.count(l) != t.count(l):
+        s_dict = defaultdict(int)
+        for c in s:
+            s_dict[c] += 1
+        for c in t:
+            if c not in s_dict.keys():
+                return False
+            s_dict[c] -= 1
+            if s_dict[c] < 0:
                 return False
         return True
