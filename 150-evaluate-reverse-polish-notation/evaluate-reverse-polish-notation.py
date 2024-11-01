506635413993b2ee479845662cf37c4d7bf4469d
Mohamed Esmail <moh.ahmed.esmail.1403@gmail.com>
2024-11-01 09:19:50 +0200

    Time: 0 ms (100.00%) | Memory: 18.5 MB (6.49%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -4,9 +4,7 @@ class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
         for c in tokens:
-            if c.isdigit() or c.startswith('-') and c[1:].isdigit():
-                stack.append(int(c))
-            elif c in {"+", "-", "*", "/"}:
+            if c in {"+", "-", "*", "/"}:
                 op2 = stack.pop()
                 op1 = stack.pop()
                 if c == "+":
@@ -22,4 +20,7 @@ class Solution:
                     else:
                         res = int(math.ceil(res)) 
                 stack.append(res)
+            else:
+                stack.append(int(c))
+
         return stack[0]
